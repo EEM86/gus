c123f7498c68fbe5a283ba66ce6bba4f50aa6391
YYermolenko <yevhen.yermolenko-external@sema4genomics.com>
2023-01-17 11:10:56 +0200

    add animation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+from os import listdir
 from pygame. constants import QUIT, K_DOWN, K_UP, K_LEFT, K_RIGHT
 
 pygame.init()
@@ -16,23 +17,25 @@ BLUE = 0, 0, 255
 
 font = pygame.font.SysFont('Verdana', 20)
 
+ANIMATION_PATH = 'animation'
 
 main_surface = pygame.display.set_mode(screen)
 
-player = pygame.image.load('player.png').convert_alpha()
+player_animation = [pygame.image.load(ANIMATION_PATH + '/' + file).convert_alpha() for file in listdir(ANIMATION_PATH)]
+player = player_animation[0]
 player_rect = player.get_rect()
 player_speed = 5
 
 def create_enemy():
-    enemy = pygame.image.load('enemy.png').convert_alpha()
+    enemy = pygame.image.load('enemy.png')
     enemy_rect = pygame.Rect(width, random.randint(0, height), *enemy.get_size())
     enemy_speed = random.randint(2, 5)
     return [enemy, enemy_rect, enemy_speed]
 
 def create_bonus():
-    bonus = pygame.image.load('bonus.png').convert_alpha()
+    bonus = pygame.image.load('bonus.png')
     bonus_rect = pygame.Rect(random.randint(0, width), 0, *bonus.get_size())
-    bonus_speed = random.randint(4, 6)
+    bonus_speed = random.randint(1, 6)
     return [bonus, bonus_rect, bonus_speed]
 
 bg = pygame.transform.scale(pygame.image.load('background.png').convert(), screen)
@@ -46,11 +49,14 @@ pygame.time.set_timer(CREATE_ENEMY_EVENT, 1500)
 CREATE_BONUS_EVENT = pygame.USEREVENT + 2
 pygame.time.set_timer(CREATE_BONUS_EVENT, 2000)
 
+CREATE_ANIMATION = pygame.USEREVENT + 3
+pygame.time.set_timer(CREATE_ANIMATION, 175)
+
 scores = 0
+img_index = 0
 
 enemies = []
 bonuses = []
-
 
 is_working = True
 
@@ -67,6 +73,13 @@ while is_working:
 
         if event.type == CREATE_BONUS_EVENT:
             bonuses.append(create_bonus())
+
+        if event.type == CREATE_ANIMATION:
+            img_index += 1
+            if img_index == len(player_animation):
+                img_index = 0
+            player = player_animation[img_index]
+            
 
     pressed_key = pygame.key.get_pressed()
 
@@ -97,6 +110,7 @@ while is_working:
             enemies.pop(enemies.index(enemy))
 
         if player_rect.colliderect(enemy[1]):
+            scores = 0
             is_working = False
 
     for bonus in bonuses:
@@ -122,7 +136,4 @@ while is_working:
     if pressed_key[K_LEFT] and not player_rect.left <= 0:
         player_rect = player_rect.move(-player_speed, 0)
     
-    print()
-
-
     pygame.display.flip()
