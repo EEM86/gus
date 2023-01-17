e00dec14fabee639643d5ae2ad625ede7ac7c4db
YYermolenko <yevhen.yermolenko-external@sema4genomics.com>
2023-01-17 10:48:21 +0200

    add images

--- main.py
+++ main.py
@@ -12,33 +12,41 @@ BLACK = 0, 0, 0
 WHITE = 255, 255, 255
 RED = 255, 0, 0
 GREEN = 0, 255, 0
+BLUE = 0, 0, 255
+
+font = pygame.font.SysFont('Verdana', 20)
+
 
 main_surface = pygame.display.set_mode(screen)
 
-ball = pygame.Surface((20, 20))
-ball.fill(WHITE)
-ball_rect = ball.get_rect()
-ball_speed = 5
+player = pygame.image.load('player.png').convert_alpha()
+player_rect = player.get_rect()
+player_speed = 5
 
 def create_enemy():
-    enemy = pygame.Surface((20, 20))
-    enemy.fill(RED)
+    enemy = pygame.image.load('enemy.png').convert_alpha()
     enemy_rect = pygame.Rect(width, random.randint(0, height), *enemy.get_size())
     enemy_speed = random.randint(2, 5)
     return [enemy, enemy_rect, enemy_speed]
 
 def create_bonus():
-    bonus = pygame.Surface((20, 20))
-    bonus.fill(GREEN)
+    bonus = pygame.image.load('bonus.png').convert_alpha()
     bonus_rect = pygame.Rect(random.randint(0, width), 0, *bonus.get_size())
     bonus_speed = random.randint(4, 6)
     return [bonus, bonus_rect, bonus_speed]
+
+bg = pygame.transform.scale(pygame.image.load('background.png').convert(), screen)
+bg_start_position = 0
+bg_moves = bg.get_width()
+bg_speed = 3
 
 CREATE_ENEMY_EVENT = pygame.USEREVENT + 1
 pygame.time.set_timer(CREATE_ENEMY_EVENT, 1500)
 
 CREATE_BONUS_EVENT = pygame.USEREVENT + 2
 pygame.time.set_timer(CREATE_BONUS_EVENT, 2000)
+
+scores = 0
 
 enemies = []
 bonuses = []
@@ -62,9 +70,24 @@ while is_working:
 
     pressed_key = pygame.key.get_pressed()
 
-    main_surface.fill(BLACK)
+    # main_surface.blit(bg, (0, 0))
 
-    main_surface.blit(ball, ball_rect)
+    bg_start_position -= bg_speed
+    bg_moves -= bg_speed
+
+    if bg_start_position < -bg.get_width():
+        bg_start_position = bg.get_width()
+
+    if bg_moves < -bg.get_width():
+        bg_moves = bg.get_width()
+
+    main_surface.blit(bg, (bg_start_position, 0))
+    main_surface.blit(bg, (bg_moves, 0))
+
+    
+    main_surface.blit(player, player_rect)
+
+    main_surface.blit(font.render(str(scores), True, BLUE), (width - 30, 0))
 
     for enemy in enemies:
         enemy[1] = enemy[1].move(-enemy[2], 0)
@@ -73,8 +96,8 @@ while is_working:
         if enemy[1].left < 0:
             enemies.pop(enemies.index(enemy))
 
-        if ball_rect.colliderect(enemy[1]):
-            enemies.pop(enemies.index(enemy))
+        if player_rect.colliderect(enemy[1]):
+            is_working = False
 
     for bonus in bonuses:
         bonus[1] = bonus[1].move(0, bonus[2])
@@ -83,22 +106,23 @@ while is_working:
         if bonus[1].bottom > height:
             bonuses.pop(bonuses.index(bonus))
 
-        if ball_rect.colliderect(bonus[1]):
+        if player_rect.colliderect(bonus[1]):
             bonuses.pop(bonuses.index(bonus))
+            scores += 1
 
-    if pressed_key[K_DOWN] and not ball_rect.bottom >= height:
-        ball_rect = ball_rect.move(0, ball_speed)
+    if pressed_key[K_DOWN] and not player_rect.bottom >= height:
+        player_rect = player_rect.move(0, player_speed)
 
-    if pressed_key[K_UP] and not ball_rect.top <= 0:
-        ball_rect = ball_rect.move(0, -ball_speed)
+    if pressed_key[K_UP] and not player_rect.top <= 0:
+        player_rect = player_rect.move(0, -player_speed)
 
-    if pressed_key[K_RIGHT] and not ball_rect.right >= width:
-        ball_rect = ball_rect.move(ball_speed, 0)
+    if pressed_key[K_RIGHT] and not player_rect.right >= width:
+        player_rect = player_rect.move(player_speed, 0)
 
-    if pressed_key[K_LEFT] and not ball_rect.left <= 0:
-        ball_rect = ball_rect.move(-ball_speed, 0)
+    if pressed_key[K_LEFT] and not player_rect.left <= 0:
+        player_rect = player_rect.move(-player_speed, 0)
     
-
+    print()
 
 
     pygame.display.flip()
